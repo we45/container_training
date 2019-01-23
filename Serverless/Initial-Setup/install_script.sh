81a4ec8258ae88b8b7cc31adf4e2ecad665565ff
nithinjois <nithin.jois@we45.com>
2019-01-23 05:48:51 -0800

    Updated serverless-initial-setup

--- Serverless/Initial-Setup/install_script.sh
+++ Serverless/Initial-Setup/install_script.sh
@@ -13,11 +13,16 @@ apt-get install -y nodejs
 # Install virtualenv
 apt-get install -y virtualenv
 
-# Install bandit & pipenv
-pip install bandit pipenv
+# Install bandit, pipenv & awscli
+pip install bandit pipenv awscli
 
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # DVFaaS pipenv requirements
 cd /root/DVFaaS-Damn-Vulnerable-Functions-as-a-Service && pipenv --python /usr/bin/python3 --skip-lock install boto3 && cd ~/
+
+# Setup AWS
+aws configure set default.region us-east-1
+aws configure set aws_access_key_id AKIAJ5OPXKL3ER26OF2A
+aws configure set aws_secret_access_key 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
