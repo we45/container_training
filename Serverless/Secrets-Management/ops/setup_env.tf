f88f50801cbb556f813949effb1cc57b5b53cb8b
Abhay Bhargav <abhay@we45.com>
2019-03-24 10:51:30 +0800

    Added new Key Management Lab Exercise

---
+++ Serverless/Secrets-Management/ops/setup_env.tf
@@ -0,0 +1,103 @@
+resource "aws_kms_key" "test-training-key" {
+  description = "KMS Key for we45 Training"
+  enable_key_rotation = true
+}
+
+resource "random_string" "random_dynamo_table_name" {
+  length = 10
+  lower = true
+  special = false
+  upper = false
+}
+
+resource "random_string" "JWT_PASS" {
+  length = 20
+}
+
+resource "aws_dynamodb_table" "envelope-db" {
+  "attribute" {
+    name = "email"
+    type = "S"
+  }
+  hash_key = "email"
+  name = "we45-${random_string.random_dynamo_table_name.result}"
+  read_capacity = 2
+  write_capacity = 2
+}
+
+data "aws_iam_policy_document" "training_we45_enc_policy" {
+  statement {
+    actions = [
+      "dynamodb:Query",
+      "dynamodb:Scan",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem"
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.envelope-db.arn}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+
+    resources = ["${aws_kms_key.test-training-key.arn}"]
+  }
+
+  statement {
+    actions = [
+      "ssm:GetParameter",
+    ]
+    resources = ["${aws_ssm_parameter.jwt-pass.arn}"]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "envelope_policy" {
+  name = "we45-training"
+  description = "we45-training"
+  policy = "${data.aws_iam_policy_document.training_we45_enc_policy.json}"
+}
+
+resource "aws_iam_role" "envelope_role" {
+  name = "we45-training-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "envelope-role-policy" {
+  policy_arn = "${aws_iam_policy.envelope_policy.arn}"
+  role = "${aws_iam_role.envelope_role.name}"
+}
+
+resource "aws_ssm_parameter" "jwt-pass" {
+  name = "we45-training-jwt-pass"
+  type = "SecureString"
+  value = "${random_string.JWT_PASS.result}"
+}
---
+++ Serverless/Secrets-Management/training-secrets/app.py
@@ -0,0 +1,45 @@
+from chalice import Chalice
+import boto3
+from os import environ
+from uuid import uuid4
+from os import urandom
+import base64
+
+ssm = boto3.client('ssm', region_name='us-west-2')
+client = boto3.client('kms')
+dynamo = boto3.resource('dynamodb')
+
+db = environ.get('DB_TABLE')
+# jwt_pass = environ.get('JWT_PASS')
+kms_cmk = environ.get('KEY_ID')
+jwt_pwd = ssm.get_parameter(Name = 'we45-training-jwt-pass', WithDecryption = True)['Parameter']['Value']
+
+pad = lambda s: s + (32 - len(s) % 32) * b' '
+app = Chalice(app_name='training-secrets')
+
+
+def gen_key_seed():
+    data_key = client.generate_data_key(
+        KeyId=kms_cmk,
+        KeySpec="AES_256",
+    )
+    ciphertext_blob = base64.b64encode(data_key.get('CiphertextBlob')).decode()
+    plaintext_key = data_key.get('Plaintext')
+    print("Plaintext key: {}".format(plaintext_key))
+    return ciphertext_blob
+
+
+@app.route('/create-user',methods = ['POST'], content_types=['application/json'], cors = True)
+def create_user():
+    jbody = app.current_request.json_body
+    if isinstance(jbody, dict):
+        if 'email' in jbody:
+            table = dynamo.Table(db)
+            key = gen_key_seed()
+            table.put_item(
+                Item={
+                    'email': jbody['email'],
+                    'key': key
+                }
+            )
+            return {"success": "created user: {}".format(jbody['email'])}
